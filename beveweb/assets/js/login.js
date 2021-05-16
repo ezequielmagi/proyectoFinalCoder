d3ee278cb813f9d16d5476e74615e1f8d6314662
ezequiel magi <ezequielmaggi@gmail.com>
2021-05-15 23:44:25 -0300

    agregando funcionalidad al login

--- beveweb/assets/js/login.js
+++ beveweb/assets/js/login.js
@@ -5,3 +5,14 @@ abrirlogin.on("click", function (event) {
   event.preventDefault();
   loginModal.toggleClass("login-active");
 });
+
+const btnIngresar = $("#btnIngresar");
+
+btnIngresar.on("click", function (event) {
+  event.preventDefault();
+
+  const userEmail = $("#userEmail").val();
+  const userPassword = $("#userPassword").val();
+
+  console.log(userEmail, userPassword);
+});
